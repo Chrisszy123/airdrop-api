7891015b7965511777463a3514cde1309995b2ae
chrisszy123 <bassygoodluck@gmail.com>
2024-12-22 07:09:37 +0100

    fix: fixed breaking on user airdrop

--- controllers/userAirdropController.js
+++ controllers/userAirdropController.js
@@ -1,6 +1,5 @@
 const { Web3 } = require("web3");
 const axios = require("axios");
-const { erc20ABI } = require("../assets/erc20ABI");
 const { getWalletBalance } = require("../utils/getBalance");
 const Airdrop = require("../models/airdropModel");
 const {
@@ -10,33 +9,15 @@ const {
   getSubscriptions,
 } = require("../utils/boomFi");
 const DisperContract = require("../artifacts/contracts/Disperse.sol/Disperse.json");
+const { handleProvider } = require("../utils/handleChain");
+const { handleWeb3 } = require("../utils/web3");
 // Initialize Web3
 
 const processUserAirdrop = async (req, res) => {
   const { chain } = req.body;
-  let providerUrl;
-  if (chain === "polygon-amoy") {
-    providerUrl = process.env.PROVIDER_URL;
-  } else if (chain === "base") {
-    providerUrl = process.env.BASE_PROVIDER_URL;
-  } else if (chain === "polygon-mainnet") {
-    providerUrl = process.env.MAINNET_PROVIDER_URL;
-  } else {
-    providerUrl = process.env.DEFAULT_PROVIDER_URL;
-  }
   // use provider based on the chain
-  const web3 = new Web3(new Web3.providers.HttpProvider(providerUrl));
-  const tokenAddress = process.env.MUMBAI_MASQ_CONTRACT;
-
-  const tokenContract = new web3.eth.Contract(erc20ABI, tokenAddress);
-  const disperseContract = new web3.eth.Contract(
-    DisperContract.abi,
-    process.env.DISPERSE_TOKEN_ADDRESS
-  );
-  const privateKey = process.env.PRIVATE_KEY;
-
-  const account = web3.eth.accounts.privateKeyToAccount(privateKey);
-  const ownerAddress = account.address;
+  const { web3, tokenAddress, tokenContract, disperseContract, ownerAddress, privateKey } =
+    handleWeb3(chain);
 
   let userwallet = [];
   let userAmount = [];
@@ -54,9 +35,13 @@ const processUserAirdrop = async (req, res) => {
   //
   const response = await getUserByAddress();
   const idsForWallet = response.data?.data?.items
-    .filter((item) => item.customer_ident === walletAddress)
+    .filter(
+      (item) =>
+        item.customer_ident.toString().toLowerCase() ===
+        walletAddress.toString().toLowerCase()
+    )
     .map((item) => item.id);
-
+  // Check that user with wallet address exists
   if (idsForWallet.length > 0) {
     const result = await getSubscriptions();
     const userSubcription = result.data?.items.filter(
@@ -65,18 +50,19 @@ const processUserAirdrop = async (req, res) => {
         item.status === "Active" &&
         item.is_overdue === false
     );
+
     if (userSubcription.length > 0) {
       const hasMonthly = userSubcription.some(
-        (item) => item.item[0].plan.recurring_interval === "Month"
+        (item) => item.items[0].plan.recurring_interval === "Month"
       );
       const hasYearly = userSubcription.some(
-        (item) => item.item[0].plan.recurring_interval === "Year"
+        (item) => item.items[0].plan.recurring_interval === "Year"
       );
 
       // If we have both a monthly and a yearly active plan, cancel the monthly one
       if (hasMonthly && hasYearly) {
         userSubcription.forEach(async (item) => {
-          if (item.item[0].plan.recurring_interval === "Month") {
+          if (item.items[0].plan.recurring_interval === "Month") {
             // cancel subscription
             await cancelUserSubcription(item?.id);
           }
@@ -87,6 +73,8 @@ const processUserAirdrop = async (req, res) => {
     if (!web3.utils.isAddress(walletAddress)) {
       return res.status(400).json({ error: "Invalid wallet address" });
     }
+
+    //
     try {
       const balance = await getWalletBalance(
         web3,
@@ -99,45 +87,72 @@ const processUserAirdrop = async (req, res) => {
       // If balance is less than 0.5 MASQ, calculate the airdrop amount
       if (userSubcription.length > 0) {
         if (parseFloat(balance) < 0.5) {
+          // check for a new User
           if (recentAirdropsForWallet.length === 0) {
-            // check if the airdrop for the user in the last 30 days is less than 1
-            if (recentAirdropsForWallet[0].amountOfLastAirdrop < 1) {
-              const amountToAirdrop = (
-                1 -
-                parseFloat(
-                  balance + recentAirdropsForWallet[0].amountOfLastAirdrop
-                )
-              ).toString();
-              userwallet.push(walletAddress);
-              userAmount.push(web3.utils.toWei(amountToAirdrop, "ether"));
-              // tranfer native MATIC
-              if (parseFloat(balanceEth) < 0.5) {
-                const data = disperseContract.methods
-                  .disperseEther(userwallet, userAmount)
-                  .encodeABI();
+            const amountToAirdrop = (1 - parseFloat(balance)).toString();
+            userwallet.push(walletAddress);
+            userAmount.push(web3.utils.toWei(amountToAirdrop, "ether"));
+            // tranfer native MATIC
+            if (parseFloat(balanceEth) < 0.1) {
+              const valueInWei = web3.utils.toWei("0.1", "ether");
+              const data = disperseContract.methods
+                .disperseEther(userwallet, [valueInWei])
+                .encodeABI();
 
-                const tx = {
-                  from: ownerAddress,
-                  to: process.env.DISPERSE_TOKEN_ADDRESS,
-                  gas: 100000,
-                  value: web3.utils.toWei("0.1", "ether"),
-                  maxPriorityFeePerGas: web3.utils.toWei("30", "gwei"),
-                  maxFeePerGas: web3.utils.toWei("100", "gwei"),
-                  data: data,
-                };
-                const signedTx = await web3.eth.accounts.signTransaction(
-                  tx,
-                  privateKey
-                );
-                const receipt = await web3.eth.sendSignedTransaction(
-                  signedTx.rawTransaction
-                );
-                console.log(
-                  `ETH Transfer successful. Transaction receipt:`,
-                  receipt
-                );
-              }
-              if (userAmount.length > 0) {
+              const tx = {
+                from: ownerAddress,
+                to: process.env.DISPERSE_TOKEN_ADDRESS,
+                gas: 100000,
+                value: web3.utils.toWei("0.1", "ether"),
+                maxPriorityFeePerGas: web3.utils.toWei("30", "gwei"),
+                maxFeePerGas: web3.utils.toWei("100", "gwei"),
+                data: data,
+              };
+              const signedTx = await web3.eth.accounts.signTransaction(
+                tx,
+                privateKey
+              );
+              const receipt = await web3.eth.sendSignedTransaction(
+                signedTx.rawTransaction
+              );
+              console.log(`ETH Transfer successful. Transaction receipt:`);
+            }
+            if (userAmount.length > 0) {
+              const total = userAmount.reduce(
+                (acc, val) => acc + Number(val),
+                0
+              );
+              const transaction = tokenContract.methods.approve(
+                process.env.DISPERSE_TOKEN_ADDRESS,
+                total.toString()
+              );
+              // Estimate gas for the transaction
+              const gasLimit = await web3.eth.estimateGas({
+                to: tokenAddress,
+                data: transaction.encodeABI(),
+                from: ownerAddress,
+              });
+              // Create the transaction object
+              const txn = {
+                from: ownerAddress,
+                to: tokenAddress,
+                gas: gasLimit,
+                maxPriorityFeePerGas: web3.utils.toWei("30", "gwei"),
+                maxFeePerGas: web3.utils.toWei("100", "gwei"),
+                data: transaction.encodeABI(), // Encoded ABI of transferFrom method
+              };
+              // Sign the transaction with the private key
+              const signed = await web3.eth.accounts.signTransaction(
+                txn,
+                privateKey
+              );
+
+              // Send the signed transaction
+              const txnSuccess = await web3.eth.sendSignedTransaction(
+                signed.rawTransaction
+              );
+
+              if (txnSuccess.transactionHash) {
                 const data = disperseContract.methods
                   .disperseToken(tokenAddress, userwallet, userAmount)
                   .encodeABI();
@@ -163,24 +178,19 @@ const processUserAirdrop = async (req, res) => {
                   receipt
                 );
               }
-              // const receipt = await transferTokens(walletAddress, amountToAirdrop);
-              // add user airdrop to mongodb
-              const newAirdrop = new Airdrop({
-                walletAddress: walletAddress,
-                dateOfLastAirdrop: Date.now(),
-                amountOfLastAirdrop: amountToAirdrop,
-              });
-              await newAirdrop.save(); // Save the airdrop data in the database
-              return res.status(200).json({
-                message: `Airdropped ${amountToAirdrop} MASQ to ${walletAddress}`,
-                receipt,
-              });
-            } else {
-              res.status(400).json({
-                error: "User has exceeded maximum allowed amount for 30days",
-              });
             }
+            // add user airdrop to mongodb
+            const newAirdrop = new Airdrop({
+              walletAddress: walletAddress,
+              dateOfLastAirdrop: Date.now(),
+              amountOfLastAirdrop: amountToAirdrop,
+            });
+            await newAirdrop.save(); // Save the airdrop data in the database
+            return res.status(200).json({
+              message: `Airdropped ${amountToAirdrop} MASQ to ${walletAddress}`
+            });
           } else {
+            // handle transfer for old users
             res
               .status(400)
               .json({ error: "User has been airdropped in the last 30days" });
