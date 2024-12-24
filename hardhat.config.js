08200660f45bbb61383a807f791ce4197c0a6ce8
chrisszy123 <bassygoodluck@gmail.com>
2024-12-24 09:37:45 +0100

    Fix: updated hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -13,26 +13,5 @@ module.exports = {
         settings: {},
       },
     ],
-  },
-  networks: {
-    // for mainnet
-    'base-mainnet': {
-      url: 'https://mainnet.base.org',
-      accounts: [process.env.PRIVATE_KEY],
-      gasPrice: 1000000000,
-    },
-    // for testnet
-    'base-sepolia': {
-      url: 'https://sepolia.base.org',
-      accounts: [process.env.PRIVATE_KEY],
-      gasPrice: 1000000000,
-    },
-    // for local dev environment
-    'base-local': {
-      url: 'http://localhost:8545',
-      accounts: [process.env.PRIVATE_KEY],
-      gasPrice: 1000000000,
-    },
-  },
-  defaultNetwork: 'hardhat',
+  }
 };
